1350a5b2c849f1f724526fbaa5fc721ca26600e0
wclmgcd <13980437+wclmgcd@users.noreply.github.com>
2023-11-03 08:45:10 +0800

    Update script.sh

--- script.sh
+++ script.sh
@@ -6,6 +6,15 @@ sed -i 's/192.168.1.1/192.168.1.2/g' package/base-files/files/bin/config_generat
 #修改密码
 sed -i 's/^root:.*:/root:$1$q6Qf.IUu$Bd2tIMFHYYNOsmsIRBwHC0:19650:0:99999:7:::/g' package/base-files/files/etc/shadow
 
+# Add luci-app-vssr
+git clone https://github.com/jerrykuku/lua-maxminddb.git package-temp/lua-maxminddb
+git clone https://github.com/jerrykuku/luci-app-vssr.git package-temp/luci-app-vssr
+git clone https://github.com/kenzok8/small.git package-temp/small
+cp -r package-temp/small/* package/lean/
+mv -f package-temp/lua-maxminddb package/lean/
+mv -f package-temp/luci-app-vssr package/lean/
+rm -rf package-temp
+
 # Add luci-app-openclash
 git clone --depth=1 https://github.com/vernesong/OpenClash.git package-temp
 mv -f package-temp/luci-app-openclash package/lean/
