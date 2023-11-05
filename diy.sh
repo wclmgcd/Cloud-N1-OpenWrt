85c30dcc446c04314aac46f559c133e72501fcb2
wclmgcd <13980437+wclmgcd@users.noreply.github.com>
2023-11-05 23:32:38 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -6,24 +6,21 @@ sed -i 's/192.168.1.1/192.168.1.2/g' package/base-files/files/bin/config_generat
 #修改密码
 sed -i 's/^root:.*:/root:$1$q6Qf.IUu$Bd2tIMFHYYNOsmsIRBwHC0:19650:0:99999:7:::/g' package/base-files/files/etc/shadow
 
-
+rm -rf feeds/luci/applications/luci-app-homeproxy
+git clone https://github.com/douglarek/luci-app-homeproxy.git  package-temp/luci-app-homeproxy
+mv -f package-temp/luci-app-homeproxy package/
+rm -rf package-temp
 
 # Add luci-app-passwall
 git clone https://github.com/xiaorouji/openwrt-passwall-packages.git package-temp
-cp -r package-temp/* package/lean/
+cp -r package-temp/* package/
 rm -rf package-temp
 git clone https://github.com/xiaorouji/openwrt-passwall2.git package-temp
-mv -f package-temp/luci-app-passwall2 package/lean/
+mv -f package-temp/luci-app-passwall2 package/
 rm -rf package-temp
-
-# Add luci-app-openclash
-git clone --depth=1 https://github.com/vernesong/OpenClash.git package-temp
-mv -f package-temp/luci-app-openclash package/lean/
-rm -rf package-temp
-
 
 
 # Add luci-app-amlogic
 git clone https://github.com/ophub/luci-app-amlogic.git  package-temp/luci-app-amlogic
-mv -f package-temp/luci-app-amlogic/luci-app-amlogic package/lean/
+mv -f package-temp/luci-app-amlogic/luci-app-amlogic package/
 rm -rf package-temp
