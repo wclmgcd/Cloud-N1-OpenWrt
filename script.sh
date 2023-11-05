9cb0b001ea648339c4129e7c363fa2a21cee9de5
wclmgcd <13980437+wclmgcd@users.noreply.github.com>
2023-11-05 23:27:44 +0800

    Update script.sh

--- script.sh
+++ script.sh
@@ -7,38 +7,23 @@ sed -i 's/192.168.1.1/192.168.1.2/g' package/base-files/files/bin/config_generat
 sed -i 's/^root:.*:/root:$1$q6Qf.IUu$Bd2tIMFHYYNOsmsIRBwHC0:19650:0:99999:7:::/g' package/base-files/files/etc/shadow
 
 # Add luci-app-vssr
-git clone https://github.com/jerrykuku/lua-maxminddb.git package-temp/lua-maxminddb
-git clone https://github.com/jerrykuku/luci-app-vssr.git package-temp/luci-app-vssr
-git clone https://github.com/kenzok8/small.git package-temp/small
-cp -r package-temp/small/* package/lean/
-mv -f package-temp/lua-maxminddb package/lean/
-mv -f package-temp/luci-app-vssr package/lean/
+
+rm -rf /feeds/luci/applications/luci-app-homeproxy
+git clone https://github.com/douglarek/luci-app-homeproxy.git  package-temp/luci-app-homeproxy
+mv -f package-temp/luci-app-homeproxy package/
 rm -rf package-temp
 
 # Add luci-app-passwall
 git clone https://github.com/xiaorouji/openwrt-passwall-packages.git package-temp
-cp -r package-temp/* package/lean/
+cp -r package-temp/* package/
 rm -rf package-temp
 git clone https://github.com/xiaorouji/openwrt-passwall2.git package-temp
-mv -f package-temp/luci-app-passwall2 package/lean/
+mv -f package-temp/luci-app-passwall2 package/
 rm -rf package-temp
 
-# Add luci-app-openclash
-git clone --depth=1 https://github.com/vernesong/OpenClash.git package-temp
-mv -f package-temp/luci-app-openclash package/lean/
-rm -rf package-temp
-
-# Add luci-theme-opentomcat
-git clone https://github.com/Leo-Jo-My/luci-theme-opentomcat.git theme-temp/luci-theme-opentomcat
-rm -rf theme-temp/luci-theme-opentomcat/LICENSE
-rm -rf theme-temp/luci-theme-opentomcat/README.md
-mv -f theme-temp/luci-theme-opentomcat package/lean/
-rm -rf theme-temp
-default_theme='opentomcat'
-sed -i "s/bootstrap/$default_theme/g" feeds/luci/modules/luci-base/root/etc/config/luci
 
 
 # Add luci-app-amlogic
 git clone https://github.com/ophub/luci-app-amlogic.git  package-temp/luci-app-amlogic
-mv -f package-temp/luci-app-amlogic/luci-app-amlogic package/lean/
+mv -f package-temp/luci-app-amlogic/luci-app-amlogic package/
 rm -rf package-temp
