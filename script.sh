86ca5f2a3c92f175b1929664661d7ab128183a2d
wclmgcd <13980437+wclmgcd@users.noreply.github.com>
2023-12-04 12:50:33 +0800

    Update script.sh

--- script.sh
+++ script.sh
@@ -6,20 +6,14 @@ sed -i 's/192.168.1.1/192.168.1.2/g' package/base-files/files/bin/config_generat
 #修改密码
 sed -i 's/^root:.*:/root:$1$q6Qf.IUu$Bd2tIMFHYYNOsmsIRBwHC0:19650:0:99999:7:::/g' package/base-files/files/etc/shadow
 
-# Add luci-app-vssr
 
-# Add luci-app-homeproxy
-git clone https://github.com/lxiaya/openwrt-homeproxy.git package-temp
+# Add luci-app-passwall2
+git clone https://github.com/xiaorouji/openwrt-passwall-packages.git package-temp
 cp -r package-temp/* package/
 rm -rf package-temp
-
-# Add luci-app-passwall
-# git clone https://github.com/xiaorouji/openwrt-passwall-packages.git package-temp
-# cp -r package-temp/* package/
-# rm -rf package-temp
-# git clone https://github.com/xiaorouji/openwrt-passwall2.git package-temp
-# mv -f package-temp/luci-app-passwall2 package/
-# rm -rf package-temp
+git clone https://github.com/xiaorouji/openwrt-passwall2.git package-temp
+mv -f package-temp/luci-app-passwall2 package/
+rm -rf package-temp
 
 
 
