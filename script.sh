d0d4228f5aa0416b4b1b7602cddda09e0902954c
wclmgcd <13980437+wclmgcd@users.noreply.github.com>
2024-04-08 11:58:05 +0800

    Update script.sh

--- script.sh
+++ script.sh
@@ -7,6 +7,18 @@ sed -i 's/192.168.1.1/192.168.1.2/g' package/base-files/files/bin/config_generat
 sed -i 's/^root:.*:/root:$1$q6Qf.IUu$Bd2tIMFHYYNOsmsIRBwHC0:19650:0:99999:7:::/g' package/base-files/files/etc/shadow
 
 
+sudo apt update
+sudo apt install gcc-9 g++-9
+
+sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-9 20
+sudo update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-9 20
+
+sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-8 10
+sudo update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-8 10
+
+sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-7 1
+sudo update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-7 1
+
 # Add luci-app-passwall2
 # git clone https://github.com/xiaorouji/openwrt-passwall-packages.git package-temp
 # cp -r package-temp/* package/
